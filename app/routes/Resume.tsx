9813f333e640626b6d0c78fd2d7e9eee780a319b
ravneet kaur <146877258+ravneet27062004@users.noreply.github.com>
2026-09-02 01:09:31 +0530

    fix resume route filename

--- app/routes/Resume.tsx
+++ app/routes/Resume.tsx
@@ -1,9 +1,9 @@
 import {Link, useNavigate, useParams} from "react-router";
 import {useEffect, useState} from "react";
 import {usePuterStore} from "~/lib/puter";
-import Summary from "~/components/summary";
-import ATS from "~/components/ats";
-import Details from "~/components/details";
+import Summary from "~/components/Summary";
+import ATS from "~/components/ATS";
+import Details from "~/components/Details";
 
 
 export const meta = () => ([
